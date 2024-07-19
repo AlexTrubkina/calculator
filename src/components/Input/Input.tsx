812ebe4360d0361e38996dd510ad8dafaa0ab6e0
AlexTrubkina <sashatru99@gmail.com>
2024-07-19 15:13:09 +0300

    feat: add validation function to Input

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -1,8 +1,17 @@
+import { ReactElement, useState } from "react";
+import { validateInput } from "./helpers/validation";
 import "./Input.scss"
 
-const Input = (): React.ReactElement => {
+const Input = (): ReactElement => {
+
+    const [inputValue, setInputValue] = useState('');
+
+    const onInput = (value: string) => {
+        setInputValue(validateInput(value))
+    }
+
     return (
-        <input type="text" className="input"/>
+        <input type="text" value={inputValue} onChange={(e) =>onInput(e.currentTarget.value)} className="input"/>
     )
 }
 
---
+++ src/components/Input/helpers/validation.ts
@@ -0,0 +1,3 @@
+export const validateInput = (value: string): string => {
+    return value.replaceAll(/[^0-9]/g, '')
+}
