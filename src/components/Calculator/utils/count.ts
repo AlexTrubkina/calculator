f1b6b2686f438a8ec557aa46a25287bdde904815
AlexTrubkina <sashatru99@gmail.com>
2024-07-31 13:07:18 +0300

    feat: changed function to define next number

--- src/components/Calculator/utils/count.ts
+++ src/components/Calculator/utils/count.ts
@@ -5,22 +5,25 @@ export const countOperations = (inputValue: string) => {
     return operations
 }
 
-export const defineNextNumber = (operations: RegExpMatchArray, inputValue: string, i: number): number => {
-    const operationIndex = inputValue.indexOf(operations[i]);
-    if (i === operations.length - 1) {
-        return +inputValue.slice(operationIndex + 1, inputValue.length);
-    } else {
-        const indexOfNextOperation = inputValue.indexOf(operations[i + 1]);
-        return +inputValue.slice(operationIndex + 1, indexOfNextOperation);
-    }
+export const countNumbers = (inputValue: string) => {
+    const numbers: RegExpMatchArray|null = inputValue.match(/[0-9]/g);
+    return numbers
+}
+
+export const defineNextNumber = (numbers: RegExpMatchArray, i: number): number => {
+    return +numbers[i + 1]
 }
 
 export const calculateResult = (inputValue: string) => {
-    const operations: RegExpMatchArray |null = countOperations(inputValue);
+    const operations: RegExpMatchArray | null = countOperations(inputValue);
+    const numbers: RegExpMatchArray | null = countNumbers(inputValue);
     if (!operations) return inputValue;
+    if (!numbers) return '0';
     let result: number = +inputValue.slice(0, 1) || 1;
     for (let i = 0; i < operations.length; i++) {
-        const nextNumber: number = defineNextNumber(operations, inputValue, i);
+        const nextNumber: number = defineNextNumber(numbers, i);
+        console.log(nextNumber);
+        
         result = buttons.operators.find((operator) => operator.name === operations[i])!.action!(result, nextNumber);
     }
     return String(result)
