a8c3d8ca0bb382a835f555b1fc043a34b6da53b4
AlexTrubkina <sashatru99@gmail.com>
2024-07-31 13:07:54 +0300

    feat: add tests to function calculateResult

--- src/tests/count.test.ts
+++ src/tests/count.test.ts
@@ -1,7 +1,14 @@
-import { countOperations } from '@components/Calculator/utils/count';
+import { countOperations, calculateResult  } from '@components/Calculator/utils/count';
 import { expect, test } from 'vitest'
 
 test('countOperations', () => {
     expect(countOperations('23')).toEqual(null);
     expect(countOperations('2+2+2')).toEqual(['+', '+']);
+});
+
+test('calculateResult', () => {
+    expect(calculateResult('23')).toEqual('23');
+    expect(calculateResult('2+2-2')).toEqual('2');
+    expect(calculateResult('2-2-2')).toEqual('-2');
+    expect(calculateResult('2-2+2')).toEqual('2');
 });
